7a4fe93ab23565e0f31c8083a81f24c6590c3263
jcho45 <127378952+jcho45@users.noreply.github.com>
2023-03-14 01:05:13 -0400

    Jacqueline - these lines of code were written after researching and learning about Otsu's method as a way to convert our grayscale image into a binary one for easier cell identification down the line. we binarized the image using the calculated threshold. (1 hour)

--- Cellulator_final.m
+++ Cellulator_final.m
@@ -1,7 +1,17 @@
 image = imread('/Users/jacquelinecho/Downloads/cell.png');
 grayimage = rgb2gray(image);
-imshow(grayimage)
+imshow(grayimage);
 
 %using a 2D median filtering command to remove the background noise, which
 %allows us to ignore any non-cellular components in the image
-noise_removal = medfilt2(grayimage)
+noise_removal = medfilt2(grayimage);
+imshow(noise_removal)
+
+%using Otsu's method, we convert our grayscale image to a binary image to
+%allow for easier counting of cells. the first step is to calculate a
+%threshold using the command 'graythresh.'
+threshold = graythresh(noise_removal);
+
+%then, we binarize the image using the threshold.
+binary_image = imbinarize(noise_removal,threshold);
+imshow(binary_image)
