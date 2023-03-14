b229ef9a64b001e8a77bc71d3604881e048a4f3a
Jchen200 <josephine_chen@brown.edu>
2023-03-14 00:11:52 -0400

    Josephine, 30 mins: This command removes any noise in the background, which allows us to ignore the non-cellular components of the image.

--- Cellulator_final.m
+++ Cellulator_final.m
@@ -1,3 +1,7 @@
 image = imread('/Users/jacquelinecho/Downloads/cell.png');
 grayimage = rgb2gray(image);
 imshow(grayimage)
+
+%using a 2D median filtering command to remove the background noise, which
+%allows us to ignore any non-cellular components in the image
+noise_removal = medfilt2(grayimage)
