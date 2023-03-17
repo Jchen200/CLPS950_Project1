e96419786ae61d5712f8b9b6a11422d14e8ace8d
Jchen200 <josephine_chen@brown.edu>
2023-03-16 23:33:34 -0400

    Josephine, 1 hr: Jacqueline and I decided on a pseudocode. We are using a matrix to run through the pixels of the binary image and subsequently count groups of pixels with the same colors (0 or 1). We have started this code by initializing the matrix and the cell counting variable.

--- Cellulator_final.m
+++ Cellulator_final.m
@@ -22,16 +22,31 @@ threshold = graythresh(noise_removal);
 binary_image = imbinarize(noise_removal,threshold);
 imshow(binary_image)
 
+%convert the image into a matrix with zeros. we can use this matrix to
+%track the number of pixels that belong in an individual cell. then, we
+%initialize the num_cells variable to begin counting before our for loop.
+[x,y] = size(binary_image);
+matrix_image = zeros(x,y);
+num_cells = 0;
+
+
+
 
 %we need to fill in the cells as a singlular color so that it will be
 %counted as one cell.
-filled_image = imfill(~binary_image, 'holes');
-imshow(filled_image);
+%filled_image = imfill(~binary_image, 'holes');
+%imshow(filled_image);
 
 %we are outlining the edges of each cell so that individual cells will be
 %counted as one.
-edge_detection = edge(filled_image, 'sobel');
-imshow(edge_detection);
+%edge_detection = edge(binary_image, 'sobel');
+%imshow(edge_detection);
+
+
+%we need to fill in the cells as a singlular color so that it will be
+%counted as one cell.
+%filled_image = imfill(edge_detection, 'holes');
+%imshow(filled_image);
 
 %we want to find the average number of pixels in one cell. This information
 %will be used in future commands to filter through componenets in the
